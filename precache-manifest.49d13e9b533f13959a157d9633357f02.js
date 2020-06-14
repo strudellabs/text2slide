3740c03f686ff0e2e4f7a3488ef4fc3ef54d6ca9
strudellabs <54324822+strudellabs@users.noreply.github.com>
2020-06-14 13:24:15 -0400

    Add files via upload

---
+++ precache-manifest.49d13e9b533f13959a157d9633357f02.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5ccf108e7751f741045f0610f994241",
+    "url": "/text2slide/index.html"
+  },
+  {
+    "revision": "9e1c82b444818a3dc474",
+    "url": "/text2slide/static/css/main.35dc6bf6.chunk.css"
+  },
+  {
+    "revision": "f126de4f481382a519d9",
+    "url": "/text2slide/static/js/2.ba71f326.chunk.js"
+  },
+  {
+    "revision": "9e1c82b444818a3dc474",
+    "url": "/text2slide/static/js/main.354f4154.chunk.js"
+  },
+  {
+    "revision": "a1c5b82f82b039b7f0c3",
+    "url": "/text2slide/static/js/runtime~main.66b54646.js"
+  },
+  {
+    "revision": "3122abf4a3e1067926c08dee7684522d",
+    "url": "/text2slide/static/media/card-profile1-square.3122abf4.jpg"
+  },
+  {
+    "revision": "788e3d36e8c4d4120daeee6e57e7e85f",
+    "url": "/text2slide/static/media/card-profile2-square.788e3d36.jpg"
+  },
+  {
+    "revision": "1a1649171cdecd24e7a16ea1340077df",
+    "url": "/text2slide/static/media/card-profile4-square.1a164917.jpg"
+  },
+  {
+    "revision": "1f1f4900f3bc166b75f8235c2b89864f",
+    "url": "/text2slide/static/media/card-profile6-square.1f1f4900.jpg"
+  },
+  {
+    "revision": "baf6b40a654b078399e93e3d9cb6d455",
+    "url": "/text2slide/static/media/profile-bg.baf6b40a.jpg"
+  }
+]);
